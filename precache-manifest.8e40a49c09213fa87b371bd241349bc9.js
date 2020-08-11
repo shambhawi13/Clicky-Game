6924ea6e7c77dfa05a52dacbcb385b7985c69c26
Sham <shambhawi.kumari1393@gmail.com>
2020-08-11 02:06:24 -0700

    Updates

---
+++ precache-manifest.8e40a49c09213fa87b371bd241349bc9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e38f5c4ac2cb86c63f36e3e009b1a09",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "947ffe81c5401515aade",
+    "url": "/Clicky-Game/static/css/main.a2255db6.chunk.css"
+  },
+  {
+    "revision": "e568a11aea79784eb4b5",
+    "url": "/Clicky-Game/static/js/2.171dfd08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Game/static/js/2.171dfd08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "947ffe81c5401515aade",
+    "url": "/Clicky-Game/static/js/main.2a9d425f.chunk.js"
+  },
+  {
+    "revision": "f70336978153cff784ba",
+    "url": "/Clicky-Game/static/js/runtime-main.a0ce3ec2.js"
+  },
+  {
+    "revision": "2f96793ccd9dfa2a7655b32f7a4a8822",
+    "url": "/Clicky-Game/static/media/CSS3.2f96793c.png"
+  },
+  {
+    "revision": "880cced4601887aaf00b4c24a2e605d3",
+    "url": "/Clicky-Game/static/media/HTML5.880cced4.jpg"
+  },
+  {
+    "revision": "4754e0b6d08f1b3b2f1563a9743f78a3",
+    "url": "/Clicky-Game/static/media/React.4754e0b6.jpg"
+  },
+  {
+    "revision": "dffb5dcdfb1305c88e897037c6a028e8",
+    "url": "/Clicky-Game/static/media/Sass.dffb5dcd.png"
+  },
+  {
+    "revision": "b2f2fbe19a4e06441db4e0b65cbf24f1",
+    "url": "/Clicky-Game/static/media/TypeScript.b2f2fbe1.jpg"
+  },
+  {
+    "revision": "3c88177095bb30849460f96f1bf11c5e",
+    "url": "/Clicky-Game/static/media/Typescript2.3c881770.png"
+  },
+  {
+    "revision": "0438b819808386d23186b2cc75102064",
+    "url": "/Clicky-Game/static/media/angular.0438b819.jpg"
+  },
+  {
+    "revision": "3cad3d9e73a626f07e107b076f508579",
+    "url": "/Clicky-Game/static/media/mongoDB.3cad3d9e.jpg"
+  },
+  {
+    "revision": "20b4e1a82057c249e862a39ee11aa86b",
+    "url": "/Clicky-Game/static/media/node.20b4e1a8.jpg"
+  }
+]);
